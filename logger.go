938b980c9405ccf751e4904c9d4c7f88d091e000
Lars Karlslund <lars@karlslund.dk>
2024-08-06 11:03:39 +0200

    Disable caller trace in logger output

--- logger.go
+++ logger.go
@@ -12,5 +12,5 @@ var Logger zerolog.Logger
 func init() {
 	Logger = zerolog.New(
 		zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339},
-	).With().Timestamp().Caller().Logger()
+	).With().Timestamp().Logger()
 }
